96c91648e67ceb4e057d4fea47d72bc99520b981
Madhu Dadi <dadim04@iimamritsar.ac.in>
2024-03-20 11:28:52 +0530

    homepage setup, postgres config, .env-sample

--- foodOnline_main/views.py
+++ foodOnline_main/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse('Hello User')
+    # return HttpResponse('Hello User')
+    return render(request, 'home.html')
