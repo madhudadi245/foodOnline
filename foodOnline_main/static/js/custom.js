22d3dc95a47071780c20d5c2f1be4bdbc6680e86
Madhu Dadi <dadim04@iimamritsar.ac.in>
2024-03-29 18:33:27 +0530

    Dynamic Tax Module

--- foodOnline_main/static/js/custom.js
+++ foodOnline_main/static/js/custom.js
@@ -105,7 +105,8 @@ $(document).ready(function(){
                     // subtotal, tax and grand total
                     applyCartAmounts(
                         response.cart_amount['subtotal'],
-                        response.cart_amount['tax'],
+                        // response.cart_amount['tax'],
+                        response.cart_amount['tax_dict'],
                         response.cart_amount['grand_total'],
                     )
                 }
@@ -153,7 +154,8 @@ $(document).ready(function(){
 
                 applyCartAmounts(
                     response.cart_amount['subtotal'],
-                    response.cart_amount['tax'],
+                    // response.cart_amount['tax'],
+                    response.cart_amount['tax_dict'],
                     response.cart_amount['grand_total'],
                 )
 
@@ -188,7 +190,8 @@ $(document).ready(function(){
                     swal(response.status, response.message, 'success');
                     applyCartAmounts(
                         response.cart_amount['subtotal'],
-                        response.cart_amount['tax'],
+                        // response.cart_amount['tax'],
+                        response.cart_amount['tax_dict'],
                         response.cart_amount['grand_total'],
                     )
                     if(window.location.pathname == '/cart/'){
@@ -217,11 +220,19 @@ $(document).ready(function(){
     }
 
     // apply cart amounts in cart page
-    function applyCartAmounts(subtotal, tax, grand_total){
+    // function applyCartAmounts(subtotal, tax, grand_total){
+    function applyCartAmounts(subtotal, tax_dict, grand_total){
         if(window.location.pathname == '/cart/'){
             $('#subtotal').html(subtotal)
-            $('#tax').html(tax)
+            // $('#tax').html(tax)
             $('#total').html(grand_total)
+            console.log(tax_dict)
+            for(key1 in tax_dict){
+                for(key2 in tax_dict[key1]){
+                    // console.log(tax_dict[key1][key2])
+                    $('#tax-'+key1).html(tax_dict[key1][key2])
+                }
+            }
         }
     }
 
